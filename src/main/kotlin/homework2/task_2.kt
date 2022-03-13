8e8cf47c47ccc3b87ff8a16ffc1cc617677e80a5
Tatyanenko Alexey <alexdtat@gmail.com>
2022-03-13 22:00:53 +0300

    Input improvement.

--- src/main/kotlin/homework2/task_2.kt
+++ src/main/kotlin/homework2/task_2.kt
@@ -1,27 +1,24 @@
 package main.kotlin.homework2
 
 fun main() {
-    var number: Int = 0
+    var stringNumber: String?
     println("Please, input a positive integer to print all not greater primes:")
     while (true) {
         try {
-            number = readln().toInt()
-            break
-        } catch (e: java.lang.NumberFormatException) {
-            println("Cannot convert into Int. Try again:")
-        }
-    }
-
-    while (true) {
-        try {
-            val argument = number
+            stringNumber = readLine() ?: throw NullPointerException("Null input.")
+            val number = stringNumber.toInt()
             val newSieveOfEratosthenes = main.libraries.SieveOfEratosthenes.SieveOfEratosthenes(number)
             newSieveOfEratosthenes.printLesserPrimes()
             break
-        } catch (exception: IllegalArgumentException) {
-            println("Invalid argument. Try again:")
-            number = readln().toInt()
+        } catch (exception: Exception) {
+            when(exception){
+                is NullPointerException -> println("Null input. Try again:")
+                is java.lang.NumberFormatException -> println("Cannot convert into Int. Try again:")
+                is IllegalArgumentException -> println("Invalid argument. Try again:")
+                else -> throw exception
+            }
         }
+
     }
 }
 
