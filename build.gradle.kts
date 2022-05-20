0b590b3d7bb6f3e8b52c902b45f16abd332119ad
alexdtat <alexdtat@gmail.com>
2022-05-20 23:42:54 +0300

    The solution has been added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,31 @@
+import org.jetbrains.compose.compose
+
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
+    id("org.jetbrains.compose") version "1.1.0"
 }
 group = "org.example"
 version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(compose.desktop.currentOs)
+    implementation("org.jsoup:jsoup:1.14.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
 
 tasks.test {
