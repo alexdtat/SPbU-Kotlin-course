3463fd3a0a2ab6b433d8f236ab740d1d6c9edf00
alexdtat <alexdtat@gmail.com>
2022-06-01 23:33:49 +0300

    RandomListConstants object instead of global constants.

--- src/main/kotlin/libraries/mergeSort/plot/generateRandomMutableList.kt
+++ src/main/kotlin/libraries/mergeSort/plot/generateRandomMutableList.kt
@@ -1,9 +1,13 @@
 package homework.homework4and5
 
 import kotlin.random.Random
+private object RandomListConstants {
+    const val LOWER_RANDOM_BOUND = -10000
+    const val UPPER_RANDOM_BOUND = 10000
+}
 
-const val LOWER_RANDOM_BOUND = -100
-const val UPPER_RANDOM_BOUND = 100
-
-fun generateRandomMutableList(size: Int, lowerBound: Int = LOWER_RANDOM_BOUND, upperBound: Int = UPPER_RANDOM_BOUND):
-    MutableList<Int> = MutableList(size) { Random.nextInt(lowerBound, upperBound) }
+fun generateRandomMutableList(
+    size: Int,
+    lowerBound: Int = RandomListConstants.LOWER_RANDOM_BOUND,
+    upperBound: Int = RandomListConstants.UPPER_RANDOM_BOUND
+): MutableList<Int> = MutableList(size) { Random.nextInt(lowerBound, upperBound) }
